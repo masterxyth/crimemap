fcf17e92d834f95348b2bf6533796c1d401f051a
masterxyth <rizwanq@sfu.ca>
2017-05-28 20:40:12 -0700

    add_crime function

--- crimemap/crimemap.py
+++ crimemap/crimemap.py
@@ -35,5 +35,15 @@ def clear():
         print e
     return home()
 
+@app.route('/submitcrime', methods=['POST'])
+def submitcrime():
+    category = request.form.qet('category')
+    date = request.form.get("date")
+    latitude = float(request.form.get("latitude"))
+    longitude = float(request.form.get("longitude"))
+    description = request.form.get('description')
+    DB.add_crime(category, date, latitude, longitude, description)
+    return home()
+
 if __name__ == '__main__':
     app.run(port=5000, debug = True)
--- crimemap/dbhelper.py
+++ crimemap/dbhelper.py
@@ -22,9 +22,9 @@ class DBHelper:
     def add_input(self, data):
         connection = self.connect()
         try:
-            query = "INSERT INTO crimes (description) VALUES ('{}');".format(data)
+            query = "INSERT INTO crimes (description) VALUES (%s);"
             with connection.cursor() as cursor:
-                cursor.execute(query)
+                cursor.execute(query, data)
                 connection.commit()
         finally:
             connection.close()
@@ -38,3 +38,16 @@ class DBHelper:
                 connection.commit()
         finally:
             connection.close()
+
+    def add_crime(self, category, date, latitude, longitude, description):
+        connection = self.connect()
+        try:
+            query = "INSERT INTO crimes (category, date, latitude, longitude, description)\
+                                        VALUES (%s,%s,%s,%s,%s)"
+            with connection.cursor() as cursor:
+                cursor.execute(query, (category, date, latitude, longitude,description))
+                connection.commit()
+            except Exception as e:
+                print e
+            finally:
+                connection.close()
